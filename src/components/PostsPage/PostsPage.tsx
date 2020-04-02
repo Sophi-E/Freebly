47841a73c4c6cfcc4a9cb42447ae54c8f21f58a1
Sophi-E <sophierithous@gmail.com>
2020-04-02 11:27:25 +0100

    postarray

--- src/components/PostsPage/PostsPage.tsx
+++ src/components/PostsPage/PostsPage.tsx
@@ -2,43 +2,55 @@ import React from 'react';
 import PostContainer from '../../reusables/PostContainer';
 import styles from './PostsPage.module.css';
 
+const PostsArray = [
+  {
+    id: 1,
+    imageUrl: 'https://via.placeholder.com/200',
+    title: 'Wristwatch',
+    postDate: '1st April 2020',
+    location: 'Woji Estate, Port Harcourt'
+  },
+  {
+    id: 1,
+    imageUrl: 'https://via.placeholder.com/200',
+    title: 'Wristwatch',
+    postDate: '1st April 2020',
+    location: 'Woji Estate, Port Harcourt'
+  },
+  {
+    id: 1,
+    imageUrl: 'https://via.placeholder.com/200',
+    title: 'Wristwatch',
+    postDate: '1st April 2020',
+    location: 'Woji Estate, Port Harcourt'
+  },
+  {
+    id: 1,
+    imageUrl: 'https://via.placeholder.com/200',
+    title: 'Wristwatch',
+    postDate: '1st April 2020',
+    location: 'Woji Estate, Port Harcourt'
+  }
+];
 const PostsPage = () => {
   return (
-    <div className={styles.postContainer}>
-      <div className={styles.card}>
-        <PostContainer
-          title='Wristwatch'
-          postDate='1st April 2020'
-          location='Woji Estate, Port Harcourt'
-        />
+    <>
+      <div>
+        <input type='search' placeholder='search' />
       </div>
-
-      <div className={styles.card}>
-        <img src='https://via.placeholder.com/200' alt='' />
-        <PostContainer
-          title='Wristwatch'
-          postDate='1st April 2020'
-          location='Woji Estate, Port Harcourt'
-        />
+      <div className={styles.postContainer}>
+        {PostsArray.map(post => (
+          <div className={styles.card}>
+            <PostContainer
+              imageUrl={post.imageUrl}
+              title={post.title}
+              postDate={post.postDate}
+              location={post.location}
+            />
+          </div>
+        ))}
       </div>
-
-      <div className={styles.card}>
-        <img src='https://via.placeholder.com/200' alt='' />
-        <PostContainer
-          title='Wristwatch'
-          postDate='1st April 2020'
-          location='Woji Estate, Port Harcourt'
-        />
-      </div>
-      <div className={styles.card}>
-        <img src='https://via.placeholder.com/200' alt='' />
-        <PostContainer
-          title='Wristwatch'
-          postDate='1st April 2020'
-          location='Woji Estate, Port Harcourt'
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/reusables/PostContainer.tsx
+++ src/reusables/PostContainer.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import styles from './PostContainer.module.css';
 
 type PostItemProps = {
-  image?: HTMLImageElement;
+  imageUrl: string;
   title: string;
   postDate: string;
   location: string;
 };
 
 const PostContainer: React.FC<PostItemProps> = ({
-  image,
+  imageUrl,
   title,
   postDate,
   location
 }: PostItemProps) => {
   return (
     <div>
-      <div>{image}</div>
+      <img src={imageUrl} alt='' />
       <h4>{title}</h4>
       <p>Posted on: {postDate}</p>
       <p>Location: {location}</p>
