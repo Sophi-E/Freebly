b490e4629d9352659592315347fe269da44d44af
Sophi-E <sophierithous@gmail.com>
2020-04-30 14:43:05 +0100

    fixed postpage css

--- src/components/PostsPage/PostsPage.tsx
+++ src/components/PostsPage/PostsPage.tsx
@@ -5,57 +5,65 @@ import styles from './PostsPage.module.css';
 const PostsArray = [
   {
     id: 1,
-    imageUrl: 'https://via.placeholder.com/200',
+    imageUrl:
+      'https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     title: 'Wristwatch',
     postDate: '1st April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 2,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Camera',
+    postDate: '21st April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 3,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Strap bag',
+    postDate: '5th April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 4,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/1546003/pexels-photo-1546003.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Backpack',
+    postDate: '1st March 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 5,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/2081199/pexels-photo-2081199.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Backpack',
+    postDate: '5th March 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 6,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/292998/pexels-photo-292998.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Dress shoes',
+    postDate: '21st April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 7,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
-    postDate: '1st April 2020',
+    imageUrl:
+      'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Sneakers',
+    postDate: '7th April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
   {
     id: 8,
-    imageUrl: 'https://via.placeholder.com/200',
-    title: 'Wristwatch',
+    imageUrl:
+      'https://images.pexels.com/photos/1445696/pexels-photo-1445696.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    title: 'Heels',
     postDate: '1st April 2020',
     location: 'Woji Estate, Port Harcourt',
   },
@@ -79,7 +87,6 @@ const PostsPage = () => {
           </div>
         ))}
       </div>
-      <p className={styles.copyright}>@ copyright 2020</p>
     </>
   );
 };
---
+++ src/reusables/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface INav {
+  brandName: string;
+  link: string;
+}
+const Nav = () => {
+  return <div></div>;
+};
+
+export default Nav;
