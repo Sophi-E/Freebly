2c030203f42cf7bd3dd461ebc10c466972ea42fe
Sophi-E <sophierithous@gmail.com>
2020-05-02 20:20:00 +0100

    create post component

--- src/components/forms/CreatePost.tsx
+++ src/components/forms/CreatePost.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from '../forms/formStyles.module.css';
 
 const CreatePost = () => {
-  return <div>Create Post</div>;
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <form>
+        <input type='text' />
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
