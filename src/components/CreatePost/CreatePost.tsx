f5a6aaa31099805350db020934f2d0f38e32eac2
Sophi-E <sophierithous@gmail.com>
2020-05-03 18:13:56 +0100

    changed file structure

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import styles from './CreatePost.module.css';
 
 const CreatePost = () => {
   return (
     <div>
-      <h1>Create Post</h1>
-      <form>
+      <form className={styles.postForm}>
+        <h1>Create Post</h1>
         <label htmlFor='itemName'> Item Name:</label>
         <input type='text' name='itemName' />
         <label htmlFor='location'> Location:</label>
         <input type='text' name='location' />
         <label htmlFor='upload'>Upload Photos:</label>
         <input type='file' name='itemImage' id='itemImage' />
+        <label htmlFor='itemDescription'>Description</label>
+        <textarea
+          name='description'
+          id='description'
+          cols={30}
+          rows={10}
+        ></textarea>
+        <label htmlFor='comment'> Comment:</label>
+        <input type='text' name='comment' />
+        <label htmlFor='shipping'>
+          Free Shipping?
+          <input type='checkbox' name='shipping' id='shipping' />
+        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
