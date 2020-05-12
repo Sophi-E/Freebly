2bef80ba9401d0b58f8a0599b13e33b7a68b469a
Sophi-E <sophierithous@gmail.com>
2020-05-12 12:53:12 +0100

    media queries

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import styles from './home.module.css';
-import giftbox from '../../images/gift-box.png';
+import boxes from '../../images/boxes.jpg';
 import Footer from '../../reusables/Footer/Footer';
 import Nav from '../../reusables/Nav/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faGifts } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleCarry } from '@fortawesome/free-solid-svg-icons';
 const Home: React.FC = () => {
   return (
     <>
       <Nav />
       <div className={styles.homeContainer}>
-        <h1>All things have value</h1>
-        <p>
-          Choose, today, to give something of value to someone at no extra cost
-        </p>
-        <img src={giftbox} alt='deliveryBox' />
+        <div className={styles.intro}>
+          <h1>All things have value</h1>
+          <p>
+            Choose, today, to give something of value to someone at no extra
+            cost
+          </p>
+        </div>
+
+        <img src={boxes} alt='deliveryBox' />
       </div>
       <div className={styles.iconWrapper}>
         <div>
-          <FontAwesomeIcon icon={faCoffee} />
+          <FontAwesomeIcon icon={faCoffee} className={styles.icon} />
           <p>Give away stuff you no longer need</p>
         </div>
         <div>
-          <FontAwesomeIcon icon={faCoffee} />
+          <FontAwesomeIcon icon={faPeopleCarry} className={styles.icon} />
           <p>Reduce clutter in your home and offices</p>
         </div>
         <div>
-          <FontAwesomeIcon icon={faCoffee} />
+          <FontAwesomeIcon icon={faGifts} className={styles.icon} />
           <p>Add value to others while also getting stuffs you need</p>
         </div>
       </div>
+
+      <p className={styles.cta}>Signup now to get started</p>
+
       <Footer />
     </>
   );
--- src/reusables/Footer/Footer.tsx
+++ src/reusables/Footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
           <button>Subscribe</button>
         </div>
       </div>
-      <p className={styles.copyright}>@ copyright 2020</p>
+      <p className={styles.copyright}>copyright @FREEBLY 2020</p>
     </div>
   );
 };
