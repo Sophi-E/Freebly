2fecaab8aff9af218ad56e43767d2ee20d1c3e43
Sophi-E <sophierithous@gmail.com>
2020-05-20 12:12:19 +0100

    formdata posts data

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -6,11 +6,20 @@ const CreatePost = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
 
-    const data = new FormData(e.target);
+    const formData = new FormData(e.target);
+    //@ts-ignore
+    // for (var [key, value] of formData.entries()) {
+    //   console.log(key, value);
+    // }
+
+    //@ts-ignore
+    console.log(...formData);
+
     axios({
       method: 'post',
       url: 'http://localhost:5000/posts',
-      data: data,
+      //@ts-ignore
+      data: [...formData],
     })
       .then(function (response) {
         //handle success
@@ -25,19 +34,12 @@ const CreatePost = () => {
     <div>
       <form id='postForm' className={styles.postForm} onSubmit={handleSubmit}>
         <h1>Create Post</h1>
+        <label htmlFor='imageUrl'>Upload Photos:</label>
+        <input type='file' name='imageUrl' id='imageUrl' />
         <label htmlFor='title'> Item Name:</label>
         <input type='text' name='title' id='title' />
         <label htmlFor='location'> Location:</label>
         <input type='text' name='location' id='location' />
-        <label htmlFor='imageUrl'>Upload Photos:</label>
-        <input type='file' name='imageUrl' id='imageUrl' />
-        <label htmlFor='itemDescription'>Description</label>
-        <textarea
-          name='description'
-          id='description'
-          cols={30}
-          rows={10}
-        ></textarea>
         <label htmlFor='comment'> Comment:</label>
         <input type='text' name='comment' id='comment' />
         <label htmlFor='shipping'>Free Shipping?</label>
