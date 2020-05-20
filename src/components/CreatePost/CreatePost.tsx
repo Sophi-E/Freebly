e847799c0b0c00db67c325e594ef1f91c75c58de
Sophi-E <sophierithous@gmail.com>
2020-05-20 13:15:26 +0100

    axios post

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -1,49 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CreatePost.module.css';
 import axios from 'axios';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [location, setLocation] = useState('');
+  const [shipping, setShipping] = useState('');
+  const [comment, setComment] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
-
-    const formData = new FormData(e.target);
-    //@ts-ignore
-    // for (var [key, value] of formData.entries()) {
-    //   console.log(key, value);
-    // }
-
-    //@ts-ignore
-    console.log(...formData);
-
+    const data = {
+      title,
+      location,
+      shipping,
+      comment,
+      imageUrl,
+      postDate: new Date().getDate,
+    };
     axios({
       method: 'post',
       url: 'http://localhost:5000/posts',
-      //@ts-ignore
-      data: [...formData],
+      data: data,
     })
       .then(function (response) {
-        //handle success
         console.log(response);
       })
       .catch(function (response) {
-        //handle error
         console.log(response);
       });
+    // setComment('');
+    // setImageUrl('');
+    // setLocation('');
+    // setTitle('');
+    // setShipping('');
   };
   return (
     <div>
       <form id='postForm' className={styles.postForm} onSubmit={handleSubmit}>
         <h1>Create Post</h1>
         <label htmlFor='imageUrl'>Upload Photos:</label>
-        <input type='file' name='imageUrl' id='imageUrl' />
+        <input
+          type='file'
+          name='imageUrl'
+          id='imageUrl'
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
         <label htmlFor='title'> Item Name:</label>
-        <input type='text' name='title' id='title' />
+        <input
+          type='text'
+          name='title'
+          id='title'
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label htmlFor='location'> Location:</label>
-        <input type='text' name='location' id='location' />
+        <input
+          type='text'
+          name='location'
+          id='location'
+          onChange={(e) => setLocation(e.target.value)}
+        />
         <label htmlFor='comment'> Comment:</label>
-        <input type='text' name='comment' id='comment' />
+        <input
+          type='text'
+          name='comment'
+          id='comment'
+          onChange={(e) => setComment(e.target.value)}
+        />
         <label htmlFor='shipping'>Free Shipping?</label>
-        <input type='text' name='shipping' id='shipping' />
+        <input
+          type='text'
+          name='shipping'
+          id='shipping'
+          onChange={(e) => setShipping(e.target.value)}
+        />
 
         <button className={styles.postBtn}>Submit</button>
       </form>
