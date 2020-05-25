79d984842ad2873948a908b77fdc1906bfeb764e
Sophi-E <sophierithous@gmail.com>
2020-05-25 10:46:47 +0100

    fixed create form css

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -41,52 +41,54 @@ const CreatePost = () => {
   return (
     <div>
       <Nav login='logout' />
-      <form id='postForm' className={styles.postForm} onSubmit={handleSubmit}>
-        <h1>Create Post</h1>
-        <label htmlFor='imageUrl'>Upload Photos:</label>
-        <input
-          type='file'
-          name='imageUrl'
-          id='imageUrl'
-          multiple
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-        />
-        <label htmlFor='title'> Item Name:</label>
-        <input
-          type='text'
-          name='title'
-          id='title'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label htmlFor='location'> Location:</label>
-        <input
-          type='text'
-          name='location'
-          id='location'
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <label htmlFor='comment'> Comment:</label>
-        <input
-          type='text'
-          name='comment'
-          id='comment'
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-        <label htmlFor='shipping'>Free Shipping?</label>
-        <input
-          type='text'
-          name='shipping'
-          id='shipping'
-          value={shipping}
-          onChange={(e) => setShipping(e.target.value)}
-        />
+      <div className={styles.formContainer}>
+        <form id='postForm' className={styles.postForm} onSubmit={handleSubmit}>
+          <h1>Create Post</h1>
+          <label htmlFor='imageUrl'>Upload Photos:</label>
+          <input
+            type='file'
+            name='imageUrl'
+            id='imageUrl'
+            multiple
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+          <label htmlFor='title'> Item Name:</label>
+          <input
+            type='text'
+            name='title'
+            id='title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label htmlFor='location'> Location:</label>
+          <input
+            type='text'
+            name='location'
+            id='location'
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <label htmlFor='comment'> Comment:</label>
+          <input
+            type='text'
+            name='comment'
+            id='comment'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <label htmlFor='shipping'>Free Shipping?</label>
+          <input
+            type='text'
+            name='shipping'
+            id='shipping'
+            value={shipping}
+            onChange={(e) => setShipping(e.target.value)}
+          />
 
-        <button className={styles.postBtn}>Submit</button>
-      </form>
+          <button className={styles.postBtn}>Submit</button>
+        </form>
+      </div>
       <Footer />
     </div>
   );
