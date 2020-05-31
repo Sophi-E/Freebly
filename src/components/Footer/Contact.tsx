2f3b0325e2a9be1e87ecd7a80a12fe91486d2047
Sophi-E <sophierithous@gmail.com>
2020-05-31 16:49:09 +0100

    contact css

--- src/components/Footer/Contact.tsx
+++ src/components/Footer/Contact.tsx
@@ -5,28 +5,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPhone,
   faMapMarkerAlt,
-  faLocationArrow,
   faMousePointer,
 } from '@fortawesome/free-solid-svg-icons';
+import styles from './FooterLinks.module.css';
 
 const Contact = () => {
   return (
     <>
       <Nav logout='LOGOUT' />
-      <div>
+      <div className={styles.contactWrapper}>
         <h4>For more info, contact us</h4>
         <div>
-          <FontAwesomeIcon icon={faPhone} />
+          <FontAwesomeIcon icon={faPhone} className={styles.contactIcon} />
           <p>Phone</p>
           <p>+2349059146315</p>
         </div>
         <div>
-          <FontAwesomeIcon icon={faMapMarkerAlt} />
+          <FontAwesomeIcon
+            icon={faMapMarkerAlt}
+            className={styles.contactIcon}
+          />
           <p>Address</p>
           <p>#26, Woji Estate, Port Harcourt. Rivers State, Nigeria</p>
         </div>
         <div>
-          <FontAwesomeIcon icon={faMousePointer} />
+          <FontAwesomeIcon
+            icon={faMousePointer}
+            className={styles.contactIcon}
+          />
           <p>Email</p>
           <p>info@freebli.com</p>
         </div>
