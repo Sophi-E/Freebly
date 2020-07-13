f736e147b335759ba4c1bc46a29a110bc096ac5f
Sophi-E <sophierithous@gmail.com>
2020-07-13 16:11:28 +0100

    edited footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Footer.module.css';
+import styled from '@emotion/styled';
+
+const StyledBio = styled.div``;
+const SocialLinks = styled.div``;
 
 const Footer = () => {
   return (
     <div>
       <div className={styles.bottomNav}>
+        <StyledBio>
+          <p>
+            FREEBLI. is a non-profit that gives you an opportunity to declutter
+            your home while also giving other people things that'll be of use to
+            them
+          </p>
+        </StyledBio>
         <div className={styles.footerLinks}>
           <ul>
             <Link to='/contact'>Contact Us</Link>
@@ -13,10 +25,15 @@ const Footer = () => {
             <Link to='/terms'>Terms of Use</Link>
           </ul>
         </div>
-        <div className={styles.subscribeBtn}>
-          <input className='input' type='email' placeholder='Newsletter' />
-          <button>Subscribe</button>
-        </div>
+        <SocialLinks>
+          <p>Follow us</p>
+          <p>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </p>
+        </SocialLinks>
       </div>
       <p className={styles.copyright}>copyright @FREEBLI. 2020</p>
     </div>
