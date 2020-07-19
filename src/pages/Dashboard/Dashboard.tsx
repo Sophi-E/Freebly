9cf63ed8155bc2f810bdabe8b4e08387ec3cf435
Sophi-E <sophierithous@gmail.com>
2020-07-19 22:14:00 +0100

    dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,19 +1,9 @@
-//@ts-nocheck
-
 import React, { useContext } from 'react';
-// import Config from '../../config';
-
 import { AuthContext } from '../../components/Auth';
-
-import classes from './dashboard.module.css';
 import { Link } from 'react-router-dom';
-import Footer from '../../components/Footer';
-// import { PostsArray } from '../PostsPage/PostsPage';
-
-//import PostContainer from '../../reusables/PostsContainer/PostContainer';
-
 import { User } from '../../datatypes/User';
 import { useHistory } from 'react-router-dom';
+import Layout from '../../components/layout';
 
 const Dashboard: React.FC = () => {
   const history = useHistory();
@@ -22,31 +12,7 @@ const Dashboard: React.FC = () => {
     history.push(`/`);
   };
   let { currentUser } = useContext<Partial<User>>(AuthContext);
-  return (
-    <>
-      <div className={classes.dashboardContainer}>
-        {/* <div className={classes.avi}>
-          <button onClick={() => DataStore.signOut()}>Logout</button>
-        </div> */}
-        <div className={classes.profile}>
-          <div className={classes.avatar}></div>
-          <p>{currentUser.displayName}</p>
-          <div className={classes.actions}>
-            <Link to='/create-post' className={classes.createBtn}>
-              Create Post
-            </Link>
-            <Link to='/view-posts' className={classes.viewBtn}>
-              View All Post
-            </Link>
-          </div>
-        </div>
-        <p>Recent Posts</p>
-
-        <p className={styles.delAcct}>Delete Account?</p>
-      </div>
-      <Footer />
-    </>
-  );
+  return <Layout></Layout>;
 };
 
 export default Dashboard;
