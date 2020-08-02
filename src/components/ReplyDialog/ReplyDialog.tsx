fc627d214567f7149ab3ef34d69834ad9d475a58
Sophi-E <sophierithous@gmail.com>
2020-08-02 17:19:32 +0100

    temp hack for requesting

--- src/components/ReplyDialog/ReplyDialog.tsx
+++ src/components/ReplyDialog/ReplyDialog.tsx
@@ -5,6 +5,7 @@ import 'antd/dist/antd.css';
 
 import { AuthContext } from '../Auth';
 import { signInViaGoogle, addReply } from '../../services/firestore';
+import { useHistory } from 'react-router-dom';
 
 type replyProps = {
   trigger?: any;
@@ -22,6 +23,7 @@ const ReplyDialog: FunctionComponent<replyProps> = ({ title, postId }) => {
     setIsVisible(false);
   };
 
+  const history = useHistory();
   const okHandler = () => {
     const data = {
       message,
@@ -30,6 +32,7 @@ const ReplyDialog: FunctionComponent<replyProps> = ({ title, postId }) => {
     console.log(data);
     addReply(postId)(data);
     setIsVisible(false);
+    history.push('/thank-you');
   };
   const editMessageHandler = (event: any) => {
     setMessage(event.target.value);
