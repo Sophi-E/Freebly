ec58217e62517cca3c567ec8b76d1e0e86979a51
Sophi-E <sophierithous@gmail.com>
2020-08-08 22:33:18 +0100

    mod(formsubmitted-page)

--- src/components/FormSubmittedPage.tsx
+++ src/components/FormSubmittedPage.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
+import Box from '../images/value.svg';
 
 const StyledDiv = styled.div`
   max-width: 700px;
-  margin: 7rem auto;
+  margin: 5rem auto;
   text-align: center;
 
+  /* .imgCon {
+    width: 500px;
+  } */
+  img {
+    width: 100%;
+  }
   h1 {
-    padding: 1em 0;
+    padding: 0.5em 0;
+  }
+  a {
+    text-decoration: underline;
   }
 `;
 const FormSubmittedPage: React.FC = () => {
   return (
     <StyledDiv>
+      <div className='imgCon'>
+        <img src={Box} alt='box' />
+      </div>
       <h1>Your post has been submitted</h1>
       <p>
         Go back to the <Link to='/'>homepage</Link> or keep exploring latest{' '}
