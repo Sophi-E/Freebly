09075bc5a846f58599385c6beebbc8a195c2439c
sophi-e <sophierithous@gmail.com>
2020-09-22 21:51:27 +0100

    fix weird white space on contact page

--- todo.ts
+++ todo.ts
@@ -1,22 +1,3 @@
-//get all UIs working
-//add nav and footer to desired pages
-//make nav and footer standalone components
-//fix css
-//connect firebase firestore
-//functionality
-//api calls
-//fix error messages
-//add spinner
-//dark mode
-//database
-//crud
-//test
-
-//DONE
-//fix flex issue on post page
-//integrate firebase >> done
-//authentication >> done
-
 // 06-28, Toby
 /****
  * Made Nav (login/logout) extensible, support a callback to run after login
